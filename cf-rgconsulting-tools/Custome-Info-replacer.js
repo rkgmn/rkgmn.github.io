855885200a05e09f81d8bab70f65d87388b4c065
rkgmn <65090144+rkgmn@users.noreply.github.com>
2022-03-29 03:07:04 +0530

    Update and rename Spit-full-name.js to Custome-Info-replacer.js

--- cf-rgconsulting-tools/Custome-Info-replacer.js
+++ cf-rgconsulting-tools/Custome-Info-replacer.js
@@ -9,5 +9,5 @@
 * Version: 1.0
 **************************/
 console.log('%cFunnel powered by CF PRO TOOLS™️','font-weight: bold; font-size: 16px; color: #1372B0;');
-console.log('%cNot a valid referring domain for add on: c05f1921-fa13-42d1-8b32-a13921c5b0e8','font-weight: bold; font-size: 16px; color: red;');
+console.log('%cNot a valid referring domain for add on: a60c0515-4dc2-495c-b0d7-e6b2c431b172','font-weight: bold; font-size: 16px; color: red;');
 console.log('%cIf you are the site owner, please login at https://app.cfprotools.com and add this domain to your account','font-weight: bold; font-size: 16px; color: red;');
