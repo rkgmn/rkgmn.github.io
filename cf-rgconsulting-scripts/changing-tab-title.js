18258db3f441ffd52b8703572369218c568c099e
rkgmn <65090144+rkgmn@users.noreply.github.com>
2022-03-31 22:46:23 +0530

    Update changing-tab-title.js

--- cf-rgconsulting-scripts/changing-tab-title.js
+++ cf-rgconsulting-scripts/changing-tab-title.js
@@ -1,44 +1,74 @@
-var betaFeatures = false;
-var kScript_3 = true,
-ps3_kUserScript = "28152",
-ps3_cTabTitle = "â¤ WE MISS YOU â¤";
-console.log('%cCF rgconsulting Scripts', 'color:#bd1b1e;font-size:40px;font-weight:bold;font-family:Aria;');console.log('%chttps://www.rahulconsulting.com', 'color:#000;font-size:16px;font-weight:bold;font-family:Aria;');console.log('%cAdding rgconsulting Script: CF Tab Title', 'color:green;');
-var pageTitle = document.title;
+ps58_kUserScript = "11110",
+ps58_cTabTitle = "ðŸ‘‹ Hey There...",
+ps58_cTabTitle2 =,
+ps58_nInterval = "1";var kScript_18 = true,
+var ps58_pageTitle = document.title;
+var ps58_interval = 0;
+var ps58_blinkInterval = 0;
 
-function ps3_onFocus() {
-    document.title = pageTitle;
-}
-function ps3_onBlur() {
-    document.title = ps3_cTabTitle;
+function ps58_blink(title1, title2, timeout) {
+  timeout = timeout || 1000;
+
+  document.title = title1;
+  // fix character issue
+  if (document.title != title1) {
+    title1 = document.title;
+  }
+
+  ps58_blinkInterval = setInterval(() => {
+    if (document.title == title1) {
+      document.title = title2;
+      // fix character issue
+      if (document.title != title1) {
+        title2 = document.title;
+      }
+    } else {
+      document.title = title1;
+    }
+  }, timeout);
 }
 
-var ps3_interval;
-var ps3_embedFocused;
-window.focus();      // I needed this for events to fire afterwards initially  
-addEventListener('focus', function (e) {
-    // global window focused
-    if (ps3_embedFocused) {
+function ps58_onFocus() {
+  document.title = ps58_pageTitle;
+  clearInterval(ps58_blinkInterval);
+}
+function ps58_onBlur() {
+  if (
+    typeof ps58_cTabTitle2 !== "undefined" &&
+    ps58_cTabTitle2.replace(/  +/g, " ").length > 0
+  ) {
+    ps58_blink(ps58_cTabTitle, ps58_cTabTitle2, ps58_nInterval * 1000);
+  } else {
+    ps58_blink(ps58_cTabTitle, ps58_pageTitle, ps58_nInterval * 1000);
+  }
+}
+
+var ps58_interval;
+var ps58_embedFocused;
+window.focus(); // I needed this for events to fire afterwards initially
+addEventListener("focus", function (e) {
+  // global window focused
+  if (ps58_embedFocused) {
+    // embedded element lost focus
+    ps58_embedFocused = false;
+    clearInterval(ps58_interval);
+  } else ps58_onFocus();
+});
+
+addEventListener("blur", function (e) {
+  // global window lost focus
+  if (document.hasFocus()) {
+    // embedded element is focused
+    ps58_embedFocused = true;
+    ps58_interval = setInterval(() => {
+      if (!document.hasFocus()) {
         // embedded element lost focus
-        ps3_embedFocused = false;
-        clearInterval(ps3_interval);
-    }
-    else ps3_onFocus();
+        window.focus();
+        ps58_embedFocused = false;
+        ps58_onBlur();
+        clearInterval(ps58_interval);
+      }
+    }, 100);
+  } else ps58_onBlur();
 });
-
-addEventListener('blur', function (e) {
-    // global window lost focus
-    if (document.hasFocus()) {
-        // embedded element is focused
-        ps3_embedFocused = true;
-        ps3_interval = setInterval(() => {
-            if (!document.hasFocus()) {
-                // embedded element lost focus
-                window.focus();
-                ps3_embedFocused = false;
-                ps3_onBlur();
-                clearInterval(ps3_interval);
-            }
-        }, 100);
-    }
-    else ps3_onBlur();
-});
+    
