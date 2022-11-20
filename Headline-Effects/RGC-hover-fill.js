8cbc52051e2f66c4f7ebc1aaa5a72fd0452880fc
rkgmn <65090144+rkgmn@users.noreply.github.com>
2022-11-20 19:06:27 +0530

    Update RGC-hover-fill.js

--- Headline-Effects/RGC-hover-fill.js
+++ Headline-Effects/RGC-hover-fill.js
@@ -9,15 +9,20 @@
 * Version: 1.0
 ****************************/
 
-function pictureHeadline() {
-    $('[data-title*="rgconsulting-image-headline-container"]')['each'](function() {
-        var _0x898fx2 = $(this);
-        var _0x898fx3 = _0x898fx2['css']('background-image');
-        _0x898fx2['addClass']('noBg');
-        var _0x898fx4 = $(this)['find']('[data-title*="rgconsulting-image-headline"] .elHeadline');
-        _0x898fx4['each'](function() {
-            $(this)['css']('background-image', _0x898fx3)
+function hoverFill() {
+    $('[data-title="rgconsulting-hover-fill"]')['each'](function() {
+        var _0xd99cx2 = $(this);
+        var _0xd99cx3 = _0xd99cx2['find']('.elHeadline');
+        var _0xd99cx4 = _0xd99cx3['css']('background-color');
+        var _0xd99cx5 = _0xd99cx3['css']('text-align');
+        _0xd99cx2['css']({
+            "\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x2D\x63\x6F\x6C\x6F\x72": '',
+            "\x74\x65\x78\x74\x2D\x61\x6C\x69\x67\x6E": _0xd99cx5
+        });
+        _0xd99cx3['css']({
+            "\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x2D\x63\x6F\x6C\x6F\x72": '',
+            "\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x2D\x69\x6D\x61\x67\x65": 'linear-gradient(to right, transparent 50%, ' + _0xd99cx4 + ' 50%)'
         })
     });
-    $('head')['append']('<style>[data-title*="rgconsulting-image-headline"] .elHeadline{-webkit-background-clip: text; -webkit-text-fill-color: transparent; background-clip: text; background-position:center;}.noBg{background: none !important;}</style>')
+    $('head')['append']('<style>[data-title="rgconsulting-hover-fill"] .elHeadline{background-size: 200%;transition: all 1s ease-in-out;display: inline;}[data-title="rgconsulting-hover-fill"] .elHeadline:hover{background-position: -100%;cursor: pointer;}</style>')
 }
