548bb3ed1b1a3b54fab21308d9c6841cb0c9867b
rkgmn <65090144+rkgmn@users.noreply.github.com>
2022-11-20 18:04:23 +0530

    Create RGC-glowing-headline.js

---
+++ Headline-Effects/RGC-glowing-headline.js
@@ -0,0 +1,11 @@
+/**************************
+* Copyright 2022 Rahul Consulting. All Rights Reserved.
+* Do not share, or distribute this code without the author's consent.
+* This copyright notice must remain in place whenever using
+* this code - DO NOT REMOVE
+* Author: Rahul Gupta
+* Website: https://rahulconsulting.com
+* Add-on: RGC Glowing headline
+* Version: 1.0
+**************************/
+var _0xaae7=["\x2E\x65\x6C\x48\x65\x61\x64\x6C\x69\x6E\x65","\x66\x69\x6E\x64","\x63\x6F\x6C\x6F\x72","\x63\x73\x73","\x62","\x74\x65\x78\x74\x2D\x73\x68\x61\x64\x6F\x77","\x30\x20\x30\x20\x31\x30\x70\x78\x20","\x65\x61\x63\x68","\x5B\x64\x61\x74\x61\x2D\x74\x69\x74\x6C\x65\x2A\x3D\x22\x72\x67\x63\x6F\x6E\x73\x75\x6C\x74\x69\x6E\x67\x2D\x67\x6C\x6F\x77\x2D\x68\x65\x61\x64\x6C\x69\x6E\x65\x22\x5D","\x30\x20\x30\x20\x35\x70\x78\x20","\x5B\x64\x61\x74\x61\x2D\x74\x69\x74\x6C\x65\x2A\x3D\x22\x63\x66\x64\x74\x2D\x67\x6C\x6F\x77\x2D\x68\x65\x61\x64\x6C\x69\x6E\x65\x22\x5D"];function glowHeadline(){$(_0xaae7[8])[_0xaae7[7]](function(){var _0x8dfdx2=$(this);var _0x8dfdx3=_0x8dfdx2[_0xaae7[1]](_0xaae7[0]);var _0x8dfdx4=_0x8dfdx3[_0xaae7[3]](_0xaae7[2]);var _0x8dfdx5=_0x8dfdx3[_0xaae7[1]](_0xaae7[4])[_0xaae7[3]](_0xaae7[2]);_0x8dfdx3[_0xaae7[3]](_0xaae7[5],_0xaae7[6]+ _0x8dfdx4);_0x8dfdx3[_0xaae7[1]](_0xaae7[4])[_0xaae7[3]](_0xaae7[5],_0xaae7[6]+ _0x8dfdx5)})}var _0x50e8=[_0xaae7[0],_0xaae7[1],_0xaae7[2],_0xaae7[3],_0xaae7[4],_0xaae7[5],_0xaae7[6],_0xaae7[9],_0xaae7[7],_0xaae7[10]];function glowHeadline(){$(_0x50e8[9])[_0x50e8[8]](function(){var _0x8dfdx7=$(this);var _0x8dfdx8=_0x8dfdx7[_0x50e8[1]](_0x50e8[0]);var _0x8dfdx9=_0x8dfdx8[_0x50e8[3]](_0x50e8[2]);var _0x8dfdxa=_0x8dfdx8[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[2]);_0x8dfdx8[_0x50e8[3]](_0x50e8[5],_0x50e8[6]+ _0x8dfdx9);_0x8dfdx8[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[5],_0x50e8[7]+ _0x8dfdxa)})}
