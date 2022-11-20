c8f1d7d9dfb7cd11e23b2b91b620420dffa9f1c2
rkgmn <65090144+rkgmn@users.noreply.github.com>
2022-11-20 21:29:01 +0530

    Update RGC-glowing-headline.js

--- Headline-Effects/RGC-glowing-headline.js
+++ Headline-Effects/RGC-glowing-headline.js
@@ -8,25 +8,4 @@
 * Add-on: RGC Glowing headline
 * Version: 1.0
 **************************/
-function glowHeadline() {
-    $('[data-title*="rgconsulting-glow-headline"]')['each'](function() {
-        var _0x2709x2 = $(this);
-        var _0x2709x3 = _0x2709x2['find']('.elHeadline');
-        var _0x2709x4 = _0x2709x3['css']('color');
-        var _0x2709x5 = _0x2709x3['find']('b')['css']('color');
-        _0x2709x3['css']('text-shadow', '0 0 10px ' + _0x2709x4);
-        _0x2709x3['find']('b')['css']('text-shadow', '0 0 10px ' + _0x2709x5)
-    })
-}
-var _0x50e8 = ["\x2E\x65\x6C\x48\x65\x61\x64\x6C\x69\x6E\x65", "\x66\x69\x6E\x64", "\x63\x6F\x6C\x6F\x72", "\x63\x73\x73", "\x62", "\x74\x65\x78\x74\x2D\x73\x68\x61\x64\x6F\x77", "\x30\x20\x30\x20\x31\x30\x70\x78\x20", "\x30\x20\x30\x20\x35\x70\x78\x20", "\x65\x61\x63\x68", "\x5B\x64\x61\x74\x61\x2D\x74\x69\x74\x6C\x65\x2A\x3D\x22\x63\x66\x64\x74\x2D\x67\x6C\x6F\x77\x2D\x68\x65\x61\x64\x6C\x69\x6E\x65\x22\x5D"];
-
-function glowHeadline() {
-    $(_0x50e8[9])[_0x50e8[8]](function() {
-        var _0x1167x2 = $(this);
-        var _0x1167x3 = _0x1167x2[_0x50e8[1]](_0x50e8[0]);
-        var _0x1167x4 = _0x1167x3[_0x50e8[3]](_0x50e8[2]);
-        var _0x1167x5 = _0x1167x3[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[2]);
-        _0x1167x3[_0x50e8[3]](_0x50e8[5], _0x50e8[6] + _0x1167x4);
-        _0x1167x3[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[5], _0x50e8[7] + _0x1167x5)
-    })
-}
+var _0xc743=["\x2E\x65\x6C\x48\x65\x61\x64\x6C\x69\x6E\x65","\x66\x69\x6E\x64","\x63\x6F\x6C\x6F\x72","\x63\x73\x73","\x62","\x74\x65\x78\x74\x2D\x73\x68\x61\x64\x6F\x77","\x30\x20\x30\x20\x31\x30\x70\x78\x20","\x65\x61\x63\x68","\x5B\x64\x61\x74\x61\x2D\x74\x69\x74\x6C\x65\x2A\x3D\x22\x72\x67\x63\x6F\x6E\x73\x75\x6C\x74\x69\x6E\x67\x2D\x67\x6C\x6F\x77\x2D\x68\x65\x61\x64\x6C\x69\x6E\x65\x22\x5D","\x30\x20\x30\x20\x35\x70\x78\x20","\x5B\x64\x61\x74\x61\x2D\x74\x69\x74\x6C\x65\x2A\x3D\x22\x63\x66\x64\x74\x2D\x67\x6C\x6F\x77\x2D\x68\x65\x61\x64\x6C\x69\x6E\x65\x22\x5D"];function glowHeadline(){$(_0xc743[8])[_0xc743[7]](function(){var _0x33e4x2=$(this);var _0x33e4x3=_0x33e4x2[_0xc743[1]](_0xc743[0]);var _0x33e4x4=_0x33e4x3[_0xc743[3]](_0xc743[2]);var _0x33e4x5=_0x33e4x3[_0xc743[1]](_0xc743[4])[_0xc743[3]](_0xc743[2]);_0x33e4x3[_0xc743[3]](_0xc743[5],_0xc743[6]+ _0x33e4x4);_0x33e4x3[_0xc743[1]](_0xc743[4])[_0xc743[3]](_0xc743[5],_0xc743[6]+ _0x33e4x5)})}var _0x50e8=[_0xc743[0],_0xc743[1],_0xc743[2],_0xc743[3],_0xc743[4],_0xc743[5],_0xc743[6],_0xc743[9],_0xc743[7],_0xc743[10]];function glowHeadline(){$(_0x50e8[9])[_0x50e8[8]](function(){var _0x33e4x7=$(this);var _0x33e4x8=_0x33e4x7[_0x50e8[1]](_0x50e8[0]);var _0x33e4x9=_0x33e4x8[_0x50e8[3]](_0x50e8[2]);var _0x33e4xa=_0x33e4x8[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[2]);_0x33e4x8[_0x50e8[3]](_0x50e8[5],_0x50e8[6]+ _0x33e4x9);_0x33e4x8[_0x50e8[1]](_0x50e8[4])[_0x50e8[3]](_0x50e8[5],_0x50e8[7]+ _0x33e4xa)})}
